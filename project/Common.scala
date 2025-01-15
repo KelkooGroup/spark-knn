33cc542883044f24e6c2b2db246a0304b3b8d7ce
Andrea Baita <andrea.baita@kelkoo.com>
2025-01-15 11:04:19 +0100

    Update organisation, add sonatype plugin

--- project/Common.scala
+++ project/Common.scala
@@ -1,11 +1,16 @@
-import com.typesafe.sbt.GitVersioning
+import com.github.sbt.git.GitVersioning
 import sbt._
 import Keys._
-import com.typesafe.sbt.GitPlugin.autoImport._
+import com.github.sbt.git.GitPlugin.autoImport._
+import xerial.sbt.Sonatype.autoImport._
+import xerial.sbt.Sonatype.sonatypeCentralHost
 
 object Common {
   val commonSettings = Seq(
-    ThisBuild / organization := "com.github.saurfang",
+    ThisBuild / organization := "com.kelkoogroup",
+    ThisBuild / homepage := Some(url("https://github.com/KelkooGroup/spark-knn")),
+    ThisBuild / sonatypeCredentialHost := sonatypeCentralHost,
+    sonatypeRepository := "https://s01.oss.sonatype.org/service/local",
     javacOptions ++= Seq("-source", "11", "-target", "11"),
     scalacOptions ++= Seq("-deprecation", "-feature"),
     git.useGitDescribe := true,
